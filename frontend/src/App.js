6c47cea2cd6e8b5bded637c50288c71026e28b7e
marcio-eduardo <marcioeduardo.ti@gmail.com>
2024-11-06 13:12:42 -0300

    Primeira etapa
    Signed-off-by: marcio-eduardo <marcioeduardo.ti@gmail.com>

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <h1>Projeto de Extensão Web Back-End</h1>
+        <h3>Este é um exemplo de uma Single Page Application (SPA) usando React.</h3>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
